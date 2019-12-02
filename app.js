716071f8b5d4b759b32d08feae17e0a738193900
Sharon Deloach <deloach_sharon@columbusstate.edu>
2019-12-02 18:41:38 +0000

    attempt2

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ var path = require('path');
 const express = require("express");
 var bodyParser = require("body-parser");
 const app = express();
-const port = process.env.PORT || 3000;
 const fetch = require('node-fetch');
-
+const port = process.env.PORT || 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ encoded: false}));
+app.use(bodyParser.urlencoded({encoded: false}));
+
 
 // var task = ["bark", "meow"];
 // var complete = ["eat", "sleep"];
@@ -21,30 +21,33 @@ var cTitle = '';
 var year = '';
 var image = '';
 
+
+
 app.get('/', function(req, res)
 {
     // res.render("index");
-    getComic(true);
-    res.render('index',{title: cTitle, year: year, image: image});
+    fetch('https://xkcd.com/info.0.json')
+    .then(res => res.json())
+     .then(data => {
+         year = data.year;
+        cTitle = data.title;
+        image = data.img;
+        });
+    res.render('index',{ctitle: cTitle, year: year, image: image});
 });
+
 app.get('/randomComic', function(req, res)
 {
-   getComic(false);
-   res.render('index',{title:cTitle, year:year, image:image});
+   fetch('http://xkcd.com/'+Math.floor((Math.random() * 2220) + 1)+'/info.0.json')
+   .then(res => res.json())
+   .then(data => {
+        year = data.year;
+        cTitle = data.title;
+        image = data.img;
 });
-function getComic(something){
-    var comic = 'https://xkcd.com/info.0.json';
-    //var randNum = rand(1,2208);
-    var randComic = 'http://xkcd.com/'+ rand(1,2208) + '/info.0.json';
-    fetch(something ? comic : randComic)
-        .then(res => res.json())
-        .then(json =>{
-        year = json.year;
-        cTitle = json.title;
-        image = json.img;
-            //console.log(json);
-        });
-}
+    res.render('index2',{ctitle: cTitle, year: year, image: image});
+});
+
 http.createServer(app).listen(port, function()
 {
 
