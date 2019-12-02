1e4800b224dee361d33eaa5cb89cf5357ed9455f
Sharon Deloach <deloach_sharon@columbusstate.edu>
2019-12-02 19:10:59 +0000

    attempt7

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ var path = require('path');
 const express = require("express");
 var bodyParser = require("body-parser");
 const app = express();
-const fetch = require('node-fetch');
 const port = process.env.PORT || 3000;
+const fetch = require('node-fetch');
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({encoded: false}));
-
+app.use(bodyParser.urlencoded({ encoded: false}));
 
 // var task = ["bark", "meow"];
 // var complete = ["eat", "sleep"];
